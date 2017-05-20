e4d500e19a0af547e818e0e3a115c86881b33a82
24taufikkur <taufik_24@icloud.com>
2017-05-20 20:57:59 +0700

    Initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl517/surgafilm/fragment/SoonFragment.java
@@ -0,0 +1,31 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl517.surgafilm.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.sch.smktelkom_mlg.privateassignment.xirpl517.surgafilm.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SoonFragment extends Fragment {
+
+
+    public SoonFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_soon, container, false);
+    }
+
+}
